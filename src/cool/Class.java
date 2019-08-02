8769ea4077763d55e6dfd1f92bc019d68dbf9033
MustafaErolSaglam <mustafaerol_34@hotmail.com>
2019-08-02 01:15:59 -0500

    I have added the subtraction method

--- src/cool/Class.java
+++ src/cool/Class.java
@@ -10,4 +10,7 @@ public class Class {
     public static double add( double num1, double num2){
         return (num1+num2);
     }
+    public static  int sub(int  num1, int num2){
+        return ( num1-num2);
+    }
 }
