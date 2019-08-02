bd055026e732c2868a6ac6e9767fa8769437ad14
MustafaErolSaglam <mustafaerol_34@hotmail.com>
2019-08-02 01:05:13 -0500

    write add method

---
+++ src/cool/Class.java
@@ -0,0 +1,13 @@
+package cool;
+
+public class Class {
+
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        add(2.9,20);
+    }
+
+    public static double add( double num1, double num2){
+        return (num1+num2);
+    }
+}
